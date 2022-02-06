0b7cb3d088e733806d7bc851bfc52017fcaed522
reklama-expert@yandex.ru <reklama-expert@yandex.ru>
2022-02-06 21:52:53 +0300

    almost done

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,45 @@
-console.log('Hello world!');
+window.onload = function(){
+  
+  let popup = document.querySelector('.popup');
+  
+  let form = document.querySelector('.popup__container');
+  let formInputs = form.querySelectorAll('.popup__input');
+  
+  let profile = document.querySelector('.profile')
+  let name = profile.querySelector('.profile__title').textContent;
+  let job = profile.querySelector('.profile__subtitle').textContent;
+  let editProfileButton = profile.querySelector('.profile__edit-button');
+  
+  editProfileButton.addEventListener('click', togglePopup);
+  
+ 
+  
+
+  
+  function togglePopup(){
+    popup.classList.toggle('popup_opened');
+    name = profile.querySelector('.profile__title').textContent;
+    job = profile.querySelector('.profile__subtitle').textContent;    
+    formInputs[0].value = name;
+    formInputs[1].value = job;
+  };
+  
+  function formSubmitHandler(evt){
+    evt.preventDefault();
+    let newNameValue = formInputs[0].value;
+    let newJobValue = formInputs[1].value;
+    profile.querySelector('.profile__title').textContent = newNameValue;
+    profile.querySelector('.profile__subtitle').textContent = newJobValue;
+
+    popup.classList.remove('popup_opened');
+
+  }
+
+  
+  form.addEventListener('submit', formSubmitHandler);
+
+
+  
+
+ 
+};
