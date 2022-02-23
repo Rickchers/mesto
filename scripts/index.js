ddf18aa66cef1d4c78c2f65bea6c4f9924faa1c2
reklama-expert@yandex.ru <reklama-expert@yandex.ru>
2022-02-23 23:50:12 +0300

    to review pr5_2

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,31 @@
 //поп-ап с формой
-const popup = document.querySelector('.popup');
+const popup = document.querySelector('#editInfo');
+const closeIconEditProfile = popup.querySelector('.popup__close-button');
+
+//поп-ап добавление "Добавить карточку"
+const popupAddCard = document.querySelector('#addCard');
+const closeIconAddCardPopup = popupAddCard.querySelector('.popup__close-button');
+//форма поп-апа "Добавить карточку"
+const formAddCard = popupAddCard.querySelector('#addCardForm')
+//поле ввода имени карточки
+const cardName = popupAddCard.querySelector('#cardName');
+//поле ввода адреса картинки карточки
+const cardLink = popupAddCard.querySelector('#cardLink');
+
 //поп-ап с картинкой
 const popupPreview = document.querySelector('#popup-preview');
-//все кнопки закрытия поп-апов
-const closeIcon = document.querySelectorAll('.popup__close-button');
-//заголовок поп-апа с формой
-const popupTitle = popup.querySelector('.popup__title');
-//форма поп-апа
-const form = popup.querySelector('.popup__form');
-//поля формы поп-апа в документе
+const closeIconPreview = popupPreview.querySelector('.popup__close-button');
+
+//форма поп-апа "Редактировать профиль"
+const formEditProfile = popup.querySelector('#editInfoForm');
+
+
+
+
+//поля формы поп-апа "Редактировать профиль" в документе
 const formUserNameField = document.querySelector('#username');
 const formUserJobField = document.querySelector('#userjob');
+
 //картинка поп-апа
 const popupImage = document.querySelector('.popup__image');
 //подпись картинки поп-апа
@@ -22,9 +37,9 @@ const profile = document.querySelector('.profile')
 //кнопка добавить карточку
 const addCardButton = profile.querySelector('.profile__add-button');
 //заголовок профиля (имя пользователя)
-const UserName = profile.querySelector('.profile__title');
+const userName = profile.querySelector('.profile__title');
 //подзаголовок профиля (род занятий пользователя)
-const UserJob = profile.querySelector('.profile__subtitle');
+const userJob = profile.querySelector('.profile__subtitle');
 //кнопка "редактировать профиль"
 const editProfileButton = profile.querySelector('.profile__edit-button');
 
@@ -62,112 +77,105 @@ const initialCards = [
 ];
 //для всех елементов массива применяется функция добавления на страницу
 initialCards.forEach((item) => {
-  addCard(item.name, item.link);
+  cards.append(createCard(item.name, item.link));
 });
 
-//функция добавления карточки на страницу принимает три аргумента:
-//первые два это имя карточки и адрес её картинки, третий необязательный
-//аргумент определяет ставить карточку в конец или в начало блока
-function addCard(cardTitleValue, cardImageLinkValue, setterPrependCard){
+
+
+//функция создания карточки
+function createCard(cardTitleValue, cardImageLinkValue){
   const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
 
   cardItem.querySelector('.card__title').textContent = cardTitleValue;
   cardItem.querySelector('.card__image').src = cardImageLinkValue;
+  cardItem.querySelector('.card__image').alt = cardTitleValue;
   cardItem.querySelector('.card__heart').addEventListener('click', toggleLiked);
   cardItem.querySelector('.card__remove-button').addEventListener('click', removeCard);
   cardItem.querySelector('.card__image').addEventListener('click', preview);
-
-  if (setterPrependCard){
-    cards.prepend(cardItem);
-  } else {
-    cards.append(cardItem);
-  }
+  return cardItem;
 }
 
 //переключение состояний значка "лайк"
-function toggleLiked(event){  
+function toggleLiked(event) {
   const itemElement = event.target;
   itemElement.classList.toggle('card__heart_active');
 }
 //удаление карточки
-function removeCard(event){
+function removeCard(event) {
   const itemElement = event.target.closest('.card');
   itemElement.remove();
 }
 //закрытие поп-апа
-function closePopup(popup){
-  popup.classList.toggle('popup_opened');
+function closePopup() {
+  document.querySelector('.popup_opened').classList.remove('popup_opened');
 };
 //открытие поп-апа
-function openPopup(popup){
-  popup.classList.add('popup_opened');  
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 };
+
 //функция обработчик события submit формы поп-апа редактирования профиля
-function formSubmitHandler(event){
-  event.preventDefault();  
+function saveProfileFormSubmitHandler(event) {
+  event.preventDefault();
   //здесь в текстовые значения элементов .profile__title и .profile__subtitle
   //записываются значения полей ввода формы, заполненные пользователем
-  UserName.textContent = formUserNameField.value;
-  UserJob.textContent = formUserJobField.value;
-  form.removeEventListener('submit', formSubmitHandler);  
-  closePopup(popup);
+  userName.textContent = formUserNameField.value;
+  userJob.textContent = formUserJobField.value;  
+  closePopup();
 }
+
 //функция обработчик события submit формы поп-апа добавления карточки
-function formSubmitHandlerAddCard(event){
+function saveAddCardFormSubmitHandler(event) {
   event.preventDefault();
-  curentInputValue = document.querySelector('#username').value;
-  curentInputLinkValue = document.querySelector('#userjob').value;
-  addCard(curentInputValue, curentInputLinkValue, true);
-  form.removeEventListener('submit', formSubmitHandlerAddCard);
-  closePopup(popup);
+  curentInputValue = cardName.value;
+  curentInputLinkValue = cardLink.value;
+  cards.prepend(createCard(curentInputValue, curentInputLinkValue));
+  closePopup(popupAddCard);
 }
 //функция колл-бэк на событие 'click' кнопки "редактировать профиль"
-function editProfile(){
-  formUserNameField.value = UserName.textContent;
-  formUserJobField.value = UserJob.textContent;
-  popupTitle.textContent = 'Редактировать профиль';
-  form.addEventListener('submit', formSubmitHandler);  
+function openEditInfoPopup() {
+  formUserNameField.value = userName.textContent;
+  formUserJobField.value = userJob.textContent;
+  
   openPopup(popup);
 }
+
 //функция колл-бэк на событие 'click' кнопки "добавить карточку"
-function addCardByUser(event){
-  formUserNameField.value = '';
-  formUserJobField.value = '';
-  formUserNameField.placeholder = 'Название';
-  formUserJobField.placeholder = 'Ссылка на картинку';
-  popupTitle.textContent = 'Новое место';
-  form.addEventListener('submit', formSubmitHandlerAddCard);
-  openPopup(popup);
+function openAddCardPopup() {
+  cardName.value = '';
+  cardLink.value = '';
+  openPopup(popupAddCard);
 }
 
 //создание поп-апа с картинкой
-function preview(event){
+function preview(event) {
   popupFigcaption.textContent = event.target.closest('.card').querySelector('.card__title').textContent;
   popupImage.src = event.target.closest('.card__image').src;
+  popupImage.alt = event.target.closest('.card__image').alt;
   openPopup(popupPreview);
 }
-//функция обработчик события 'click' на кнопке закрытия поп-апа
-//определяет класс родителя кнопки и передает его функции закрытия поп-апа
-function getItemClassValue (event) {
-  currentPopup = event.target.closest('.popup');
-  closePopup(currentPopup);
-}
 
-/*eventListeners*/
 
-//методом .forEach() навешивается слушатель событий на все кнопки
-//закрытия поп-апов с классом ".popup__close-button"
-//функция колл-бэк получает значение класса родительского элемента кнопки,
-//на которой произошел клик и передет его функци, которая закрывает поп-ап 
-closeIcon.forEach((userItem) => {
-  userItem.addEventListener('click', getItemClassValue);
-});
+//eventListeners
+
+//слушатель событий кнопки "закрыть" модального окна с картинкой
+closeIconPreview.addEventListener('click', closePopup);
+
 
 //слушатель событий кнопки "редактировать профиль"
-editProfileButton.addEventListener('click', editProfile);
+editProfileButton.addEventListener('click', openEditInfoPopup);
+//слушатель событий кнопки "закрыть" модального окна "редактировать профиль"
+closeIconEditProfile.addEventListener('click', closePopup);
+//слушатель событий кнопки "сохранить" модального окна "редактировать профиль"
+formEditProfile.addEventListener('submit', saveProfileFormSubmitHandler);
+
 
 //слушатель событий кнопки "добавить карточку"
-addCardButton.addEventListener('click', addCardByUser);
+addCardButton.addEventListener('click', openAddCardPopup);
+//слушатель событий кнопки "закрыть" модального окна "добавить карточку"
+closeIconAddCardPopup.addEventListener('click', closePopup);
+//слушатель событий кнопки "сохранить" модального окна "добавить карточку"
+formAddCard.addEventListener('submit', saveAddCardFormSubmitHandler);
 
 
 
